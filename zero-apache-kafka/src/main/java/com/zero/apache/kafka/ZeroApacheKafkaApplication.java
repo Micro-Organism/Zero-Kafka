2f9687674935b434849e66b2fcf0d648003c23f5
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-27 16:10:31 +0800

    feat: springboot 集成 kafka

---
+++ zero-apache-kafka/src/main/java/com/zero/apache/kafka/ZeroApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.zero.apache.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApacheKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroApacheKafkaApplication.class, args);
+    }
+
+}
---
+++ zero-apache-kafka/src/main/java/com/zero/apache/kafka/common/listener/ConsumerListener.java
@@ -0,0 +1,18 @@
+package com.zero.apache.kafka.common.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**   
+ * 消费者监听topic=testTopic的消息
+ */
+@Component
+public class ConsumerListener {
+     
+    @KafkaListener(topics = "testTopic")
+    public void onMessage(String message){
+        //insertIntoDb(buffer);//这里为插入数据库代码
+        System.out.println("message: " + message);
+    }
+
+}
